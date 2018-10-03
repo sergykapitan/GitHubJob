af8374e38d153da1bac3230391c58db0c7c03052
sergykapitan <12kaptan12@gmail.com>
2018-10-03 11:20:34 +0300

    Initial Commit

---
+++ GitHubJob/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubJob
+//
+//  Created by Sergey Koriukin on 03.10.2018.
+//  Copyright © 2018 Sergey Koriukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
